aebe2f6c13443632c5fee3214dde2d302a2335ba
Xiaoyu Xing <xingxy91@gmail.com>
2019-04-10 22:01:01 -0500

    Create plan.py

---
+++ plan.py
@@ -0,0 +1,10 @@
+'''
+1. Test with youtube-dl package
+2. Write simple python to take only the non-3rd party ads out by:
+   1). "copy debug info", monitor clipboard, check "ad_docid", (may check with Yao for JS)
+        append to youtube link, download the video
+   2). TBD
+
+
+
+'''
